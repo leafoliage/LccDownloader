a882b5ce92493bf8c4f0eafd20e6d5f74b14ff5d
Leafoliage <leafoliages@gmail.com>
2021-06-28 10:06:27 +0800

    [add] downloadVideo

---
+++ functions/downloadVideo.js
@@ -0,0 +1,23 @@
+const download = require("download")
+const getVideoName = require("./getVideoName")
+
+const downloadVideo = async (id, no, saveDir) => {
+    try {
+        return await download(`https://www.lccnet.tv/vod1/P0${id}/default.mp4`, saveDir, {
+            filename: `${no} ${await getVideoName(id)}.mp4`
+        }).then(() => new Object({
+            id,
+            no,
+            ok: true
+        }))
+    } catch (err) {
+        console.log(`no.${no} download ERROR: ${err}`)
+        return {
+            id,
+            no,
+            ok: false
+        }
+    }
+}
+
+module.exports = downloadVideo
