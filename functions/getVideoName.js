4ac51add9334e9e81f3ee52fd47dadab6e7e3d2f
Leafoliage <leafoliages@gmail.com>
2021-06-28 13:21:01 +0800

    [fix] getVideoName

--- functions/getVideoName.js
+++ functions/getVideoName.js
@@ -1,8 +1,9 @@
 const fetch = require("node-fetch")
 const { parse } = require("node-html-parser")
 
-const getVideoName = async (url) => {
-    const html = await fetch(url).then(data => data.text())
+const getVideoName = async (id) => {
+    const html = await fetch(`https://www.lccnet.tv/pages/watch.aspx?v=P0${id}`)
+        .then(data => data.text())
     const root = parse(html)
     return root.querySelector(".con").childNodes[0].rawText
 }
